0d9810946024502588e3508d3d172ab082944fe6
Almas Abdugali <55618258+AlmasF@users.noreply.github.com>
2021-05-17 11:16:50 +0600

    загрузил все, что может пригодится и буду хранить здесь

---
+++ получение показателей/Бакалавриат (грант).sql
@@ -0,0 +1,36 @@
+/****** Скрипт для команды SelectTopNRows из среды SSMS  ******/
+SELECT [StudentID]
+      ,[SpecialityID]
+	  ,[Code]
+      ,[StatusID]
+      ,[CategoryID]
+      ,[NeedsDorm]
+      ,[AltynBelgi]
+      ,[EducationTypeID]
+      ,[EducationPaymentTypeID]
+      ,[GrantTypeID]
+      ,[EducationDurationID]
+      ,[Year]
+      ,[StudyLanguageID]
+      ,[RupID]
+      ,[EntryDate]
+      ,[GPA]
+      ,[LastUpdatedBy]
+      ,[LastUpdatedOn]
+      ,[AdvisorID]
+      ,[AcademicStatusID]
+      ,[GraduatedOn]
+      ,[AcademicStatusEndsOn]
+      ,[AcademicStatusStartsOn]
+      ,[GPA_Y]
+      ,[IsPersonalDataComplete]
+      ,[HosterPrivelegeID]
+      ,[MinorSpecialityID]
+      ,[EnrollmentTypeId]
+  FROM [KazNITU].[dbo].[Edu_Students] st
+  LEFT OUTER JOIN
+  [KazNITU].[dbo].[Edu_Specialities] sp
+  ON sp.ID = st.SpecialityID
+  WHERE StatusID != 2
+  AND Code LIKE '%B%'
+  AND EducationPaymentTypeID = '1'
